fed455f57efcc565977de7cb23c22efdc00cca41
Richard <thanhle@ancient8.gg>
2024-12-13 17:02:38 +0700

    Configure for deployment

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,33 @@
 import { HardhatUserConfig } from 'hardhat/config';
 import '@nomicfoundation/hardhat-toolbox-viem';
+import { ancient8Sepolia } from 'viem/chains';
+import * as Dotenv from 'dotenv';
+
+Dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: '0.8.20',
+  networks: {
+    ancient8Testnet: {
+      url: 'https://rpcv2-testnet.ancient8.gg',
+      accounts: [process.env.PRIVATE_KEY!],
+    },
+  },
+  etherscan: {
+    apiKey: {
+      ancient8Testnet: 'ancient8Testnet', // apiKey is not required, just set a placeholder
+    },
+    customChains: [
+      {
+        network: 'ancient8Testnet',
+        chainId: 28122024,
+        urls: {
+          apiURL: 'https://api.routescan.io/v2/network/testnet/evm/28122024/etherscan',
+          browserURL: 'https://ancient8.testnet.thesuperscan.io',
+        },
+      },
+    ],
+  },
 };
 
 export default config;
